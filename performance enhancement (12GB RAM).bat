a51adce0246c3610005824c30c0a8a0cf3df4593
limeade <50957801+hookscpp@users.noreply.github.com>
2022-01-19 00:18:15 -0800

    Update performance enhancement (12GB RAM).bat

--- performance enhancement (12GB RAM).bat
+++ performance enhancement (12GB RAM).bat
@@ -1,3 +1,3 @@
-bcdedit /set increaseuserva 6000 & :: Your ram / 2 * 1000 = input
+bcdedit /set increaseuserva 6000 & :: Your RAM/Memory(in GBs) / 2 * 1000 = input
 bcdedit /set disabledynamictick yes
 bcdedit /set useplatformtick yes
