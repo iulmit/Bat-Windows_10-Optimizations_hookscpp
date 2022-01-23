e81d81ec30fb72c19d657f628a9e0944b62e04bd
limeade <50957801+hookscpp@users.noreply.github.com>
2022-01-23 04:03:36 -0800

    Add files via upload
    Extends dark mode usage along the OS. This works for Windows 10 and potentially Windows 11 & 8.

---
+++ DarkMode.bat
@@ -0,0 +1,6 @@
+@echo off
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t REG_DWORD /d 0 /f
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "SystemUsesLightTheme" /t REG_DWORD /d 0 /f
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "SystemUsesLightTheme" /t REG_DWORD /d 0 /f
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t REG_DWORD /d 0 /f
+pause
